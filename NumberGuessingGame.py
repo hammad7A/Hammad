8c9cc90c091b253e552745db5ef0e205791aa9c7
hammad7A <literaryfigure786@gmail.com>
2024-07-14 01:00:13 -0700

    Updated NumberGuessingGame.py

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -1,11 +1,22 @@
-import random
+import random,sys
+print("Welcome to number guessing game")
+Attemps = 0
+Wins = 0
 
-options = ["1","2","3"]
-user_choice = (input("Choose your number from 1 2 or 3: "))
-computer_choice = (random.choice(options))
-while user_choice not in options:
-    user_choice = (input("Please enter a valid value: "))
-if user_choice == computer_choice:
-    print("You gusessed the number!")
-else:
-    print("Try again! ")
+while True:
+    print(f"Attemps , Wins {Attemps} , {Wins}")
+    while True:
+        print("Please choose your number between 1 and 7 ")
+        print("You can quit the game if you want by pressing q")
+        playerInput = int(input("Enter your number/move here "))
+        if playerInput == "q":
+            sys.exit()
+        computerMove = random.randint(1,7)
+        if playerInput == computerMove:
+            print("You guessed the number")
+            Wins = Wins+1
+        elif playerInput != computerMove:
+            Attemps = Attemps+1
+            break
+        else:
+            print("Better luck next time")
